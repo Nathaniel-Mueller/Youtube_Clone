b564271f13be39308e0e041c60032f2b655574a3
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2023-01-31 14:59:13 -0800

    Fixed urls.py to work for POST request, added POST request to views.py, added debug file, added Postman data

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<pk>', views.videoComments)
+    path('', views.postComments),
+    path('<pk>', views.viewComments)
 ]
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -8,7 +7,21 @@ from .serializers import *
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def videoComments(request, pk):
-    comments = Comment.objects.filter(video_id=pk)
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
+def viewComments(request, pk):
+    if request.method == 'GET':
+        comments = Comment.objects.filter(video_id=pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def postComments(request):
+    print(
+    'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user = request.user)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
