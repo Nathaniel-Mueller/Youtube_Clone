97863131b6b56672b86cb13b673b97993d73d61b
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2023-01-31 14:05:30 -0800

    Added urls and admin to comment app

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializers import *
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def videoComments(request, pk):
+    comments = Comment.objects.filter(video_id=pk)
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
